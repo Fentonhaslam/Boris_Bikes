c6796778f101eeb64e1117263814cbd335aaa6f0
fentonhaslam <fentonhaslam@gmail.com>
2019-07-25 15:15:14 +0100

    bike not available error, bike available and working unit test improved

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,7 +3,8 @@ require "docking_station"
 describe DockingStation do
   it { is_expected.to respond_to(:release_bike) }
 
-  it "releases working bike" do
+  it "releases working bike when a bike available and working" do
+    subject.dock(Bike.new)
     bike = subject.release_bike
     expect(bike).to be_working
   end
@@ -21,5 +22,9 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
+  end
+
+  it "does not release bike when none are available" do
+    expect { subject.release_bike }.to raise_error("No bikes available")
   end
 end
