450f30bddfd330b76ad76e02cf59b52229b7a3ef
fentonhaslam <fentonhaslam@gmail.com>
2019-07-25 16:27:49 +0100

    added 20 bikes to dock test and created new collection - bikes to docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,19 @@
 # in lib/docking_station.rb
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-    raise "No bikes available" unless @bike
-    @bike
+    raise "No bikes available" if @bikes == []
+    @bikes.pop
   end
 
   def dock(bike)
-    raise "Cannot dock bike, station full." if @bike
-    @bike = bike
+    raise "Cannot dock bike, station full." if @bikes.length == 20
+    @bikes << bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,16 +13,17 @@ end
 
 describe DockingStation do
   it { is_expected.to respond_to(:dock).with(1).argument }
-  it { is_expected.to respond_to(:bike) }
+  it { is_expected.to respond_to(:bikes) }
   it "docks bike" do
     bike = Bike.new
-    expect(subject.dock(bike)).to eq(bike)
+    subject.dock(bike)
+    expect(subject.bikes.last).to eq(bike)
   end
 
   it "returns docked bike" do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.bikes).to eq [bike]
   end
 
   it "does not release bike when none are available" do
@@ -30,8 +31,7 @@ describe DockingStation do
   end
 
   it "does not let me dock a new bike when there's already one" do
-    subject.dock(Bike.new)
+    20.times do subject.dock(Bike.new) end
     expect { subject.dock(Bike.new) }.to raise_error("Cannot dock bike, station full.")
   end
-
 end
